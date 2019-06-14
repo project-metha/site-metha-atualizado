d9267a5e7d6c85a43b0fc76fde5d4976c829ea23
bertolucci67 <50000580+bertolucci67@users.noreply.github.com>
2019-06-14 19:46:46 -0300

    Add files via upload

---
+++ chat2.js
@@ -0,0 +1,17 @@
+(function($) {
+    $(document).ready(function() {
+        var $chatbox = $('.chatbox'),
+            $chatboxTitle = $('.chatbox__title'),
+            $chatboxCredentials = $('.chatbox__credentials');
+        $chatboxTitle.on('click', function() {
+            $chatbox.toggleClass('chatbox--tray');
+        });
+        $chatbox.on('transitionend', function() {
+            if ($chatbox.hasClass('chatbox--closed')) $chatbox.remove();
+        });
+        $chatboxCredentials.on('submit', function(e) {
+            e.preventDefault();
+            $chatbox.removeClass('chatbox--empty');
+        });
+    });
+})(jQuery);
